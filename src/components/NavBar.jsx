5771ebd2c85226a5b68b6788e855b8659c98b9d9
Henna Singh <hennasingh04@gmail.com>
2023-05-02 06:22:55 +0100

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../imgs/airbnb-logo.png'
+
+export default function NavBar() {
+
+    return (
+        <div>
+            <img className="logo" src={logo} />
+        </div>
+    )
+}
